d67063489fd84a1ebbae32c5fd8a772a21e92ef8
EC2 Default User <ec2-user@ip-172-31-55-123.ec2.internal>
2025-12-13 23:58:53 +0000

    Ajustando configuracoes

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "vini-terraform-clc14"
+    bucket       = "diego-terraform-clc14-automation"
     key          = "network/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "minha_vpc" {
 
 # Correcao primeira issue
 resource "aws_flow_log" "example" {
-  log_destination      = "arn:aws:s3:::vini-terraform-clc14"
+  log_destination      = "arn:aws:s3:::diego-terraform-clc14-automation"
   log_destination_type = "s3"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.minha_vpc.id
